0f589edf4052913aa39c6c2c1d34cce16010ec8b
MovieHub21 <135356707+MovieHub21@users.noreply.github.com>
2026-09-18 12:14:45 +0100

    Move Annex public pages into dedicated app/annex route tree

---
+++ app/annex/layout.tsx
@@ -0,0 +1,5 @@
+import PublicRouteLayout from '../(public)/layout'
+
+export default function AnnexLayout({ children }: { children: React.ReactNode }) {
+  return <PublicRouteLayout>{children}</PublicRouteLayout>
+}
