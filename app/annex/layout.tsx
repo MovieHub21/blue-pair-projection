09032954fad0bc50e761068f13def40b3ae1b5a9
MovieHub21 <135356707+MovieHub21@users.noreply.github.com>
2026-09-19 20:59:00 +0100

    refactor: remove old Annex route path

--- app/annex/layout.tsx
+++
@@ -1,5 +0,0 @@
-import PublicRouteLayout from '../(public)/layout'
-
-export default function AnnexLayout({ children }: { children: React.ReactNode }) {
-  return <PublicRouteLayout>{children}</PublicRouteLayout>
-}
