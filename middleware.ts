0dc0fd39b88c39979647bb482eb0436fdbb9e7da
MovieHub21 <135356707+MovieHub21@users.noreply.github.com>
2026-09-19 12:08:04 +0100

    fix: clean PWA matcher exclusions

--- middleware.ts
+++ middleware.ts
@@ -137,4 +137,4 @@ export async function middleware(request: NextRequest) {
   return response
 }
 
-export const config = { matcher: ['/((?!_next/static|_next/image|favicon.ico|favicon.png||.*\\.(?:png|jpg|jpeg|gif|webp|svg|ico|avif|css|js|map|woff|woff2|ttf|otf)$).*)'] }
+export const config = { matcher: ['/((?!_next/static|_next/image|favicon.ico|favicon.png|.*\\.(?:png|jpg|jpeg|gif|webp|svg|ico|avif|css|js|map|woff|woff2|ttf|otf)$).*)'] }
